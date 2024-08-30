ccbb6ccc6533fe8172caad0c29f2da9d992a2dd3
zakariae <sebaizakariae@gmail.com>
2024-08-30 17:24:30 +0200

    create benefits page

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -1,13 +1,42 @@
 import { AcademicCapIcon, HomeModernIcon, UserGroupIcon } from "@heroicons/react/24/solid"
-import { SelectedPage } from "../../shared/types"
+import { BenefitType, SelectedPage } from "../../shared/types"
 import { motion } from "framer-motion"
 import HText from "./HText"
+import Benefit from "./Benefit"
 
 
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void,
 }
+const benefits: Array<BenefitType> = [
+    {
+      icon: <HomeModernIcon className="h-6 w-6" />,
+      title: "State of the Art Facilities",
+      description:
+        "Neque adipiscing amet amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et.",
+    },
+    {
+      icon: <UserGroupIcon className="h-6 w-6" />,
+      title: "100's of Diverse Classes",
+      description:
+        "Eu ipsum id egestas risus tempus enim semper felis quis. Nec consectetur ac venenatis facilisi est. Eget ac turpis id.",
+    },
+    {
+      icon: <AcademicCapIcon className="h-6 w-6" />,
+      title: "Expert and Pro Trainers",
+      description:
+        "Fusce vestibulum aliquam ut cras. Nisl lectus egestas sapien nisl. Lacus at mi sit pellentesque. Congue parturient.",
+    },
+  ];
+
+  const container = {
+    hidden:{},
+    visible:{
+        transition: {staggerChildren:.2}
+    }
+  }
+  
 
 const Benefits = ({ setSelectedPage }: Props) => {
     return (
@@ -34,9 +63,19 @@ const Benefits = ({ setSelectedPage }: Props) => {
              {/** Benefits */}
             <motion.div
                 className="mt-5 items-center justify-between gap-8 md:flex"
+                initial='hidden'
+                whileInView='visible'
+                viewport={{once:true, amount:.5}}
+                variants={container}
             >
-                {benefits.map((benefit)=>{
-                    return <Benef
+                {benefits.map((benefit:BenefitType)=>{
+                    return <Benefit
+                            key={benefit.title}
+                            icon={benefit.icon}
+                            title={benefit.title}
+                            description={benefit.description}
+                            setSelectedPage={setSelectedPage}
+                            />
                 })}
             </motion.div>
         </section>
